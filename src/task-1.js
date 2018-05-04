2183a25282885f70680fa9901a3999013146af49
Anton Chernov <a.chernov@zaycev.net>
2018-05-04 16:40:21 +0500

    added grade and properties

--- src/task-1.js
+++ src/task-1.js
@@ -3,7 +3,25 @@ import {getGrade, getProperties} from './base';
 const api = () => {
 	const grade = getGrade();
 
+	grade
+		.then(result => {
+
+			// нужно получить все уникальные propertyId
+			const uniqPropertyIds = Object.keys(result.properties.map(grade => grade.property)
+				.reduce((acc, item) => {
+
+					acc[item] = true;
+
+					return acc;
+				}, {}));
+
+			return Promise.all([result, getProperties(uniqPropertyIds)]);
+		})
+		.then(([grades, props]) => {
+
+		});
+
 	return Promise.resolve([]);
-}
+};
 
 export default api;
