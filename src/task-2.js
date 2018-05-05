5238880f0d5aa0208bb6928bfa18b88a7f955043
Anton <anton940800@yandex.ru>
2018-05-05 18:42:18 +0500

    add tast2

--- src/task-2.js
+++ src/task-2.js
@@ -3,7 +3,33 @@ import {getGrade} from './base';
 const api = () => {
 	const grade = getGrade();
 
-	return Promise.resolve([]);
-}
+	return grade
+		.then(result => {
+			// отфильтруем лишние значения
+			return {
+				properties: result.properties.filter(prop => prop.propertyConditions)
+			}
+		})
+		.then(result => {
+			// на выходе ожидаем массив
+			return result.properties.map(grade => {
+
+				const {region, propertyConditions} = grade;
+
+				return propertyConditions.map(prop => {
+
+					const {standard} = prop;
+
+					return {
+						region,
+						standard
+					}
+				});
+			});
+		})
+		.then(([result1, result2]) => {
+			return result1.concat(result2);
+		});
+};
 
 export default api;
